e86708993f640bc2134b2dae1442355e1bb4404b
rubenruizga <e9.ruben@gmail.com>
2017-08-09 18:48:17 -0600

    Set registroSD as firebase project

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyBUioYtW5GaAEZNUEE3wgVKfrbQUf6Nmos",
-    authDomain: "webapps-2262c.firebaseapp.com",
-    databaseURL: "https://webapps-2262c.firebaseio.com",
-    projectId: "webapps-2262c",
-    storageBucket: "webapps-2262c.appspot.com",
-    messagingSenderId: "493309489055"
+    apiKey: "AIzaSyDTyPkVDNjMUh_Lig9VK4IUcC46UTdQJmM",
+    authDomain: "registrosd-21846.firebaseapp.com",
+    databaseURL: "https://registrosd-21846.firebaseio.com",
+    projectId: "registrosd-21846",
+    storageBucket: "registrosd-21846.appspot.com",
+    messagingSenderId: "208081336359"
   };
   firebase.initializeApp(config);
 } catch (e) {
